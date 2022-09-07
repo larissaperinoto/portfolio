6be3df40c03ad795550851f4ebb60f9b11cfcef6
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-07 20:52:12 -0300

    Cria dados de projetos.

---
+++ src/services/projectsData.js
@@ -0,0 +1,45 @@
+import Nikel from '../image/Nikel.png';
+import TrybeWallet from '../image/TrybeWallet.png';
+import SolarSistem from '../image/SolarSistem.png';
+import TrybeShopping from '../image/TrybeShopping.png';
+import ColorGuess from '../image/ColorGuess.png';
+import TodoList from '../image/TodoList.png';
+
+export const data = [
+  {
+    image: { Nikel },
+    name: 'Nikel Aplication',
+    url: 'https://larissaperinoto.github.io/AplicacaoNikel/',
+    repositorie: 'https://github.com/larissaperinoto/AplicacaoNikel',
+  },
+  {
+    image: { TrybeWallet },
+    name: 'TrybeWallet',
+    url: 'https://larissaperinoto.github.io/project-trybewallet/',
+    repositorie: 'https://github.com/larissaperinoto/project-trybewallet',
+  },
+  {
+    image: { SolarSistem },
+    name: 'Solar System',
+    url: 'https://larissaperinoto.github.io/project-solar-system/',
+    repositorie: 'https://github.com/larissaperinoto/project-solar-system',
+  },
+  {
+    image: { TrybeShopping },
+    name: 'Trybe Shopping',
+    url: 'https://larissaperinoto.github.io/project-shopping-cart/',
+    repositorie: 'https://github.com/larissaperinoto/project-shopping-cart',
+  },
+  {
+    image: { ColorGuess },
+    name: 'Color Guess',
+    url: 'https://larissaperinoto.github.io/project-color-guess/',
+    repositorie: 'https://github.com/larissaperinoto/project-color-guess',
+  },
+  {
+    image: { TodoList },
+    name: 'To Do List',
+    url: 'https://github.com/larissaperinoto/project-to-do-list',
+    repositorie: 'https://larissaperinoto.github.io/project-to-do-list/',
+  },
+];
