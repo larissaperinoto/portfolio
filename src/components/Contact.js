ab87b6f1e4de608d3a5f5a0c7e52346f9f93ad88
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-08 19:53:01 -0300

    Insere css no componente Contact.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,18 +4,32 @@ import '../css/contact.css';
 export default function Contact() {
   return (
     <section id="contact">
-      <a href="https://www.linkedin.com/in/larissaperinoto" title="Linkedin" target="blank">
-        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/linkedin.png" alt="Linkedin"/>
-      </a>
-      <a href="https://github.com/larissaperinoto" title="GitHub" target="blank">
-        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/github.png" alt="GitHub"/>
-      </a>
-      <a href="mailto:perinotolarissa@gmail.com" title="Email" target="blank">
-        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/mail.png" alt="Email"/>
-      </a>
-      <a href="https://drive.google.com/file/d/1Whfs3wFy3c533zsOb_31OyNxxg-5lNkh/view?usp=sharing" title="Curriculum" target="blank">
-        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/resume.png" alt="Curriculum"/>
-      </a>
+      <div className="contact_background_container">
+        <div className="contact_background_green"/>
+        <div className="contact_background_purple">
+          <h3 className="whiteTextColor">
+            {`{`}<span>Contact me </span>{`}`}
+          </h3>
+        </div>
+      </div>
+
+      <div className="contact_container">
+        <a href="https://www.linkedin.com/in/larissaperinoto" title="Linkedin" target="blank">
+          <img src="https://img.icons8.com/dusk/64/000000/linkedin--v1.png" alt="Linkedin"/>
+        </a>
+        <a href="https://github.com/larissaperinoto" title="GitHub" target="blank">
+          <img src="https://img.icons8.com/dusk/64/000000/github.png" alt="GitHub"/>
+        </a>
+        <a href="mailto:perinotolarissa@gmail.com" title="Email" target="blank">
+          <img src="https://img.icons8.com/dusk/64/000000/gmail.png" alt="Email"/>
+        </a>
+        <a href="https://drive.google.com/file/d/1Whfs3wFy3c533zsOb_31OyNxxg-5lNkh/view?usp=sharing" title="Curriculum" target="blank">
+          <img src="https://img.icons8.com/dusk/64/000000/resume.png" alt="Curriculum"/>
+        </a>
+        <a href="https://open.spotify.com/user/22wksmrmnf6bbakqayxq3koli?si=f076d97b0593428a" title="Spotify" target="blank">
+          <img src="https://img.icons8.com/dusk/64/000000/spotify.png" alt="Spotify"/>
+        </a>
+      </div>
     </section>
   );
 }
