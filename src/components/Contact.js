3d7d8ad2a2ccf55a2e2c557f94a8ca09c8d8a49b
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-08 18:49:55 -0300

    Adiciona icones e links de contato.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { FaGithub } from 'react-icons/fa';
 import '../css/contact.css';
 
 export default function Contact() {
   return (
-    <section id='contact'>
-      <a href='https://github.com/larissaperinoto'>
-        <FaGithub />
+    <section id="contact">
+      <a href="https://www.linkedin.com/in/larissaperinoto" title="Linkedin" target="blank">
+        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/linkedin.png" alt="Linkedin"/>
+      </a>
+      <a href="https://github.com/larissaperinoto" title="GitHub" target="blank">
+        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/github.png" alt="GitHub"/>
+      </a>
+      <a href="mailto:perinotolarissa@gmail.com" title="Email" target="blank">
+        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/mail.png" alt="Email"/>
+      </a>
+      <a href="https://drive.google.com/file/d/1Whfs3wFy3c533zsOb_31OyNxxg-5lNkh/view?usp=sharing" title="Curriculum" target="blank">
+        <img src="https://img.icons8.com/ios-filled/50/FFFFFF/resume.png" alt="Curriculum"/>
       </a>
     </section>
   );
