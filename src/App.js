28de1e7ed719573054d52db9ba472e9ca6f4bb76
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-08 20:05:39 -0300

    Adiciona Footer da página.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { About, Contact, Header, Projects } from './components';
+import { About, Contact, Footer, Header, Projects } from './components';
 import './css/app.css';
 
 export default function App() {
   return (
     <>
-    <Header />
-    <About />
-    <Projects />
-    <Contact />
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/footer.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Desenvolvido com React e muito CSS por Larissa. © 2022</p>
+    </footer>
+  );
+}
