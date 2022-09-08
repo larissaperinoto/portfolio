1ddf04e491833499ba1f8febeb2d0914bd84e2e9
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-08 10:47:54 -0300

    Adiciona imagem dos projetos em carrosel.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,27 @@
 import React from 'react';
+
+import { data } from '../services/projectsData';
 import '../css/projects.css';
-import Nikel from '../img/Nikel.png';
 
 export default function Projects() {
   return (
     <section id="projects">
-      <div class="projects-container">
-        <div class="project-container-menu">
-          <div class="menu">
-            <div><i class="fa-regular fa-heart"></i></div>
-            <div><i class="fa-regular fa-circle-left"></i></div>
-            <div><i class="fa-regular fa-circle-right"></i></div>
-            <div><i class="fa-regular fa-window-minimize"></i></div>
-            <div><i class="fa-regular fa-arrows-up-down-left-right"></i></div>
-          </div>
-          <img src={ Nikel } alt="Projeto Nikel" class="image" />
-        </div>
-      </div>
-      <div class="project-background">
-      </div>
-      <p class="whiteTextColor">{`{`} <span>projects</span>{`}`}</p>
+      <ul className="projects-container">
+          {
+            data.map((project, index) => (
+              <li className="slide" key={ index }>
+                <div className="menu">
+                  <div><i className="fa-regular fa-heart"></i></div>
+                  <div><i className="fa-regular fa-circle-left"></i></div>
+                  <div><i className="fa-regular fa-circle-right"></i></div>
+                  <div><i className="fa-regular fa-window-minimize"></i></div>
+                  <div><i className="fa-regular fa-arrows-up-down-left-right"></i></div>
+                </div>
+                <img src={ project.image } alt={ project.name } width="100%" />
+              </li>
+            ))
+          }
+      </ul>
     </section>
   );
 }
--- src/services/projectsData.js
+++ src/services/projectsData.js
@@ -1,43 +1,36 @@
-import Nikel from '../image/Nikel.png';
-import TrybeWallet from '../image/TrybeWallet.png';
-import SolarSistem from '../image/SolarSistem.png';
-import TrybeShopping from '../image/TrybeShopping.png';
-import ColorGuess from '../image/ColorGuess.png';
-import TodoList from '../image/TodoList.png';
-
 export const data = [
   {
-    image: { Nikel },
+    image: 'https://github.com/larissaperinoto/portifolio/blob/main/src/image/Nikel.png?raw=true',
     name: 'Nikel Aplication',
     url: 'https://larissaperinoto.github.io/AplicacaoNikel/',
     repositorie: 'https://github.com/larissaperinoto/AplicacaoNikel',
   },
   {
-    image: { TrybeWallet },
+    image: 'https://github.com/larissaperinoto/portifolio/blob/main/src/image/TrybeWallet.png?raw=true',
     name: 'TrybeWallet',
     url: 'https://larissaperinoto.github.io/project-trybewallet/',
     repositorie: 'https://github.com/larissaperinoto/project-trybewallet',
   },
   {
-    image: { SolarSistem },
+    image: 'https://github.com/larissaperinoto/portifolio/blob/main/src/image/SolarSistem.png?raw=true',
     name: 'Solar System',
     url: 'https://larissaperinoto.github.io/project-solar-system/',
     repositorie: 'https://github.com/larissaperinoto/project-solar-system',
   },
   {
-    image: { TrybeShopping },
+    image: 'https://github.com/larissaperinoto/portifolio/blob/main/src/image/TrybeShopping.png?raw=true',
     name: 'Trybe Shopping',
     url: 'https://larissaperinoto.github.io/project-shopping-cart/',
     repositorie: 'https://github.com/larissaperinoto/project-shopping-cart',
   },
   {
-    image: { ColorGuess },
+    image: 'https://github.com/larissaperinoto/portifolio/blob/main/src/image/ColorGuess.png?raw=true',
     name: 'Color Guess',
     url: 'https://larissaperinoto.github.io/project-color-guess/',
     repositorie: 'https://github.com/larissaperinoto/project-color-guess',
   },
   {
-    image: { TodoList },
+    image: 'https://github.com/larissaperinoto/portifolio/blob/main/src/image/TodoList.png?raw=true',
     name: 'To Do List',
     url: 'https://github.com/larissaperinoto/project-to-do-list',
     repositorie: 'https://larissaperinoto.github.io/project-to-do-list/',
